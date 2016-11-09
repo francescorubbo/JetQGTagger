6bfb41c6d6df3ed534478f550f5a6c46b5272b0b
francescorubbo <rubbo@atlint03.slac.stanford.edu>
2016-11-09 12:23:19 -0800

    add components

---
+++ src/components/JetQGTagger_entries.cxx
@@ -0,0 +1,13 @@
+
+#include "GaudiKernel/DeclareFactoryEntries.h"
+
+// Local include(s):
+#include "JetQGTagger/JetQGTagger.h"
+
+DECLARE_NAMESPACE_TOOL_FACTORY( CP, JetQGTagger )
+
+DECLARE_FACTORY_ENTRIES( JetQGTagger ) {
+
+   DECLARE_NAMESPACE_TOOL( CP, JetQGTagger )
+
+}
